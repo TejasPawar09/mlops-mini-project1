c57445dbbf93c21292a331611348b06b50ca12e0
Tejas <tejaspawar1000@gmail.com>
2024-09-20 20:54:17 +0530

    add flask testing code

--- flask_app/app.py
+++ flask_app/app.py
@@ -14,10 +14,6 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import dagshub
-
-mlflow.set_tracking_uri('https://dagshub.com/TejasPawar09/mlops-mini-project1.mlflow')
-dagshub.init(repo_owner='TejasPawar09', repo_name='mlops-mini-project1', mlflow=True)
 
 def lemmatization(text):
     """Lemmatize the text."""
@@ -72,7 +68,20 @@ def normalize_text(text):
     return text
 
 
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "TejasPawar09"
+repo_name = "mlops-mini-project1"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 app = Flask(__name__)
 
